1ad914d2bcebc381d359560085c175a772799e00
Blaine Lafreniere <lafrenbl@oregonstate.edu>
2026-07-06 21:15:50 -0600

    refactor

--- src/huffman.rs
+++ src/huffman.rs
@@ -1,6 +1,10 @@
-use std::collections::HashMap;
+#![allow(unused_variables)]
 
-pub struct
+use std::collections::HashMap;
+use std::env;
+use std::io::{self, IsTerminal};
+
+pub struct App;
 
 pub struct File;
 
@@ -8,27 +12,34 @@ pub struct Key {
     counts: HashMap<u8, i32>
 }
 
+impl App {
+    pub fn run() {
+        let args: Vec<String> = env::args().collect();
+
+        let encode_selected = args.iter().any(|a| a == "--encode");
+        let decode_selected = args.iter().any(|a| a == "--decode");
+
+        if !encode_selected && !decode_selected {
+            println!("Must specify either --encode or --decode");
+            std::process::exit(1);
+        }
+
+        if io::stdin().is_terminal() {
+            println!("Error: No piped input. You must provide some input via standard input.");
+            std::process::exit(1);
+        }
+
+        if encode_selected {
+            File::encode(&mut io::stdin(), &mut io::stdout());
+        } else {
+            File::decode(io::stdin(), io::stdout());
+        }
+    }
+}
+
 impl File {
-    pub fn encode(mut in_stream: impl std::io::Read, out_stream: impl std::io::Write) {
-        let mut buf = [0u8; 1024];
-        let mut bytes_read: usize = 0;
-        let mut key = Key::new();
-
-        match in_stream.read(&mut buf) {
-            Ok(val) => bytes_read = val,
-            Err(err) => {
-                println!("An error occurred: {}", err);
-                std::process::exit(1);
-            }
-        }
-
-        while bytes_read > 0 {
-            println!("bytes_read: {}", bytes_read);
-
-            key.add_to_key(&buf);
-
-            bytes_read = in_stream.read(&mut buf).unwrap();
-        }
+    pub fn encode(in_stream: &mut impl std::io::Read, out_stream: &mut impl std::io::Write) {
+        let key = Key::from_stream(in_stream);
     }
 
     pub fn decode(in_stream: impl std::io::Read, out_stream: impl std::io::Write) {
@@ -37,15 +48,41 @@ impl File {
 }
 
 impl Key {
-    pub fn new() -> Key {
-        return Key {
+    pub fn from_stream(in_stream: &mut impl std::io::Read) -> Key {
+        let mut buf = [0u8; 1024];
+        let mut bytes_read;
+
+        let mut key = Key {
             counts: HashMap::new()
         };
+
+        bytes_read = in_stream.read(&mut buf).unwrap();
+
+        #[cfg(debug_assertions)]
+        println!("bytes_read: {bytes_read}");
+
+        while bytes_read > 0 {
+            for chr in buf {
+                key.counts.entry(chr)
+                    .and_modify(|count| *count += 1)
+                    .or_insert(1);
+            }
+
+            bytes_read = in_stream.read(&mut buf).unwrap();
+
+            #[cfg(debug_assertions)]
+            println!("bytes_read: {bytes_read}");
+        }
+
+        #[cfg(debug_assertions)]
+        dbg!(&key.counts);
+
+        return key;
     }
 
-    pub fn add_to_key(&mut self, buf: &[u8]) -> () {
-        for chr in buf {
-            *self.counts.entry(*chr).or_insert(0) += 1;
+    pub fn print_counts(&self) {
+        for entry in self.counts {
+            println!("{}: {}");
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,5 @@
-use std::env;
-use std::io::{self, IsTerminal};
-
 mod huffman;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let encode_selected = args.iter().any(|a| a == "--encode");
-    let decode_selected = args.iter().any(|a| a == "--decode");
-
-    if !encode_selected && !decode_selected {
-        println!("Must specify either --encode or --decode");
-        std::process::exit(1);
-    }
-
-    if io::stdin().is_terminal() {
-        println!("Error: No piped input. You must provide some input via standard input.");
-        std::process::exit(1);
-    }
-
-    if encode_selected {
-        huffman::File::encode(io::stdin(), io::stdout());
-    } else {
-        huffman::File::decode(io::stdin(), io::stdout());
-    }
+    huffman::App::run();
 }
