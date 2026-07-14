9b47fdd9579ca6a07616d3dc6f448e86511d40bf
Blaine Lafreniere <lafrenbl@oregonstate.edu>
2026-07-14 08:02:10 -0600

    tests for: create_leaf_nodes + count_frequencies

--- src/huffman.rs
+++ src/huffman.rs
@@ -1,15 +1,29 @@
 #![allow(unused_variables)]
 
-use std::collections::HashMap;
+use std::collections::{HashMap, BinaryHeap};
+use std::cmp::Reverse;
+
 use std::env;
 use std::io::{self, IsTerminal};
 
+use uuid::Uuid;
+
 pub struct App;
 
-pub struct File;
+struct File;
 
-pub struct Key {
-    counts: HashMap<u8, i32>
+struct Key {
+    nodes: Vec<Node>,
+    root: Option<usize>
+}
+
+#[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
+struct Node {
+    weight: u32,
+    value: Option<u8>,
+    parent: Option<usize>,
+    left: Option<usize>,
+    right: Option<usize>
 }
 
 impl App {
@@ -29,17 +43,39 @@ impl App {
             std::process::exit(1);
         }
 
+        let file = Self::read_input_to_file(&mut io::stdin());
+
         if encode_selected {
             File::encode(&mut io::stdin(), &mut io::stdout());
         } else {
             File::decode(io::stdin(), io::stdout());
         }
     }
+
+    fn read_input_to_file(in_stream: &mut impl std::io::Read) -> std::fs::File {
+        let id = Uuid::new_v4();
+        let file = std::fs::File::create(format!("/tmp/{id}"))
+            .expect("Failed to save input to temporary file.");
+
+        return file;
+    }
+}
+
+impl Node {
+    pub fn new(weight: u32, value: Option<u8>) -> Node {
+        Node {
+            weight: weight,
+            value: value,
+            parent: None,
+            left: None,
+            right: None,
+        }
+    }
 }
 
 impl File {
     pub fn encode(in_stream: &mut impl std::io::Read, out_stream: &mut impl std::io::Write) {
-        let key = Key::from_stream(in_stream);
+        let key = Key::build_from_stream(in_stream);
     }
 
     pub fn decode(in_stream: impl std::io::Read, out_stream: impl std::io::Write) {
@@ -48,46 +84,95 @@ impl File {
 }
 
 impl Key {
-    /// Builds an instance of a Key from raw input.
-    pub fn create(in_stream: &mut impl std::io::Read) -> Key {
-        let mut key = Key {
-            counts: HashMap::new()
-        };
-
-        key.generate_counts(in_stream);
-
-        return key;
+    fn new() -> Key {
+        Key {
+            nodes: Vec::new(),
+            root: None
+        }
     }
 
-    /// Load a previously persisted Key from bytes.
-    pub fn load(in_stream: &mut impl std::io::Read) -> Key {
+    pub fn build_from_stream(in_stream: impl std::io::Read) {
+        let mut key = Key::new();
 
+        let counts = Key::count_frequencies(in_stream);
+
+        key.nodes = Key::create_leaf_nodes(counts);
     }
 
-    fn generate_counts(&mut self, in_stream: &mut impl std::io::Read) {
+    fn create_leaf_nodes(counts: HashMap<u8, u32>) -> Vec<Node> {
+        let mut nodes = Vec::new();
+
+        for (byte, freq) in counts {
+            let node = Node::new(freq, Some(byte));
+            nodes.push(node);
+        }
+
+        return nodes;
+    }
+
+    fn count_frequencies(mut in_stream: impl std::io::Read) -> HashMap<u8, u32> {
+        let mut counts: HashMap<u8, u32> = HashMap::new();
         let mut buf = [0u8; 1024];
         let mut bytes_read;
 
         bytes_read = in_stream.read(&mut buf).unwrap();
 
-        #[cfg(debug_assertions)]
-        println!("bytes_read: {bytes_read}");
-
         while bytes_read > 0 {
-            for chr in buf {
-                self.counts.entry(chr)
+            for byte in buf {
+                if byte == 0 { continue; }
+                counts.entry(byte)
                     .and_modify(|count| *count += 1)
                     .or_insert(1);
             }
 
             bytes_read = in_stream.read(&mut buf).unwrap();
-
-            #[cfg(debug_assertions)]
-            println!("bytes_read: {bytes_read}");
         }
 
-        #[cfg(debug_assertions)]
-        dbg!(&self.counts);
+        return counts;
+    }
+}
 
+#[cfg(test)]
+mod tests { use super::*;
+    mod key { use super::*;
+        mod count_frequencies { use super::*;
+            #[test]
+            fn test_produces_expected_counts() {
+                let input = [b'a', b'a', b'a', b'a', b'b', b'c'];
+                let counts = Key::count_frequencies(&input[..]);
+
+                assert_eq!(counts.get(&b'a'), Some(&4));
+                assert_eq!(counts.get(&b'b'), Some(&1));
+                assert_eq!(counts.get(&b'c'), Some(&1));
+            }
+
+            #[test]
+            fn test_does_not_count_null_bytes() {
+                let input = [b'a', b'a', b'a', b'a', b'b', b'c'];
+                let counts = Key::count_frequencies(&input[..]);
+
+                assert_eq!(counts.get(&0), None);
+            }
+        }
+
+        mod create_leaf_nodes { use super::*;
+            #[test]
+            fn test_returns_expected_output() {
+                let input = HashMap::from([
+                    (b'a', 5),
+                    (b'b', 10)
+                ]);
+
+                let mut nodes = Key::create_leaf_nodes(input);
+
+                let node = nodes.pop().unwrap();
+                assert_eq!(node.value, Some(b'a'));
+                assert_eq!(node.weight, 5);
+
+                let node = nodes.pop().unwrap();
+                assert_eq!(node.value, Some(b'b'));
+                assert_eq!(node.weight, 10);
+            }
+        }
     }
 }
