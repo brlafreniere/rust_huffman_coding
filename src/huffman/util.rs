41182cc534b8795e4f316efcb676367f80c8a21c
Blaine Lafreniere <brlafreniere@gmail.com>
2026-08-30 23:18:50 -0600

    remove not needed method

--- src/huffman/util.rs
+++ src/huffman/util.rs
@@ -45,38 +45,6 @@ impl<'a, W: Write> BitBuffer<'a, W> {
         self.output.write(&bytes[..])
             .expect("Could not dump bit buffer to output");
     }
-
-    pub fn as_bytes(&self) -> Vec<u8> {
-        let bytes = Vec::new();
-        let mut bits = self.buffer.clone();
-
-        // While there are bits in the buffer...
-        while bits.len() >= 0 {
-            let mut byte: u8 = 0;
-
-            // Pull a slice of 8 or less
-            let slice_len = if bits.len() >= 8 { 8 } else { bits.len() };
-            let slice = bits.drain(0..slice_len);
-
-            // For each bit in the slice, add it to our output byte
-            for bit in slice {
-                byte << 1;
-                if bit { byte += 1; }
-            }
-
-            // Handle any bit lengths < 8
-            // Example: if we have 3 bits left, then slice_len=3
-            // And we would need to pad with 8 - slice_len = 5 zero bits
-            if slice_len < 8 {
-                let pad_count = 8 - slice_len;
-                for num in 1..=pad_count {
-                    byte << 1;
-                }
-            }
-        }
-
-        return bytes;
-    }
 }
 
 #[cfg(test)]
